8f118dac567385aa67fa95ab6ae9cbaadde7fd50
Emanuele Favero <emafaux89@gmail.com>
2026-05-27 00:26:55 +0200

    feat: add exercise solution (bonus not included yet)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log('hi')
+const button = document.getElementById('toggle-button');
+const lamp = document.getElementById('lamp');
+const body = document.body;
+
+const turnOn = () => {
+  lamp.src = 'assets/img/yellow_lamp.png';
+  lamp.alt = 'Lampadina Accesa';
+  button.textContent = 'Accesa';
+  button.disabled = true;
+  body.classList.add('light-on');
+};
+
+button.addEventListener('click', turnOn);
