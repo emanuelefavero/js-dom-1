e404db6c923afc7cdfcdeecbc87828b4ade67595
Emanuele Favero <emafaux89@gmail.com>
2026-05-27 00:49:36 +0200

    feat: add bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,25 @@ const button = document.getElementById('toggle-button');
 const lamp = document.getElementById('lamp');
 const body = document.body;
 
-const turnOn = () => {
-  lamp.src = 'assets/img/yellow_lamp.png';
-  lamp.alt = 'Lampadina Accesa';
-  button.textContent = 'Accesa';
-  button.disabled = true;
-  body.classList.add('light-on');
+const ON_IMAGE = 'assets/img/yellow_lamp.png';
+const OFF_IMAGE = 'assets/img/white_lamp.png';
+
+let on = false;
+
+const toggleLamp = () => {
+  on = !on;
+
+  if (on) {
+    lamp.src = ON_IMAGE;
+    lamp.alt = 'Lampadina accesa';
+    button.textContent = 'Spegni';
+    body.classList.add('light-on');
+  } else {
+    lamp.src = OFF_IMAGE;
+    lamp.alt = 'Lampadina spenta';
+    button.textContent = 'Accendi';
+    body.classList.remove('light-on');
+  }
 };
 
-button.addEventListener('click', turnOn);
+button.addEventListener('click', toggleLamp);
